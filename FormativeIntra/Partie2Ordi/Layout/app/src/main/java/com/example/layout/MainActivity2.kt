f0f3be91cd2ac9260f1a244a9b7dd792f0986f71
Gerard <6224745@cegepmontpetit.ca>
2025-02-26 19:59:04 -0500

    FCT Mise en page et tiroir de navigation

--- FormativeIntra/Partie2Ordi/Layout/app/src/main/java/com/example/layout/MainActivity2.kt
+++ FormativeIntra/Partie2Ordi/Layout/app/src/main/java/com/example/layout/MainActivity2.kt
@@ -1,20 +1,80 @@
 package com.example.layout
 
+import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.layout.databinding.ActivityMain2Binding
+import com.example.layout.databinding.ActivityMainBinding
+import com.example.layout.databinding.NavHeaderBinding
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    private lateinit var actionBarDrawerToggle: ActionBarDrawerToggle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupDrawer()
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    private fun setupDrawer() {
+        setupDrawerApplicationBar()
+        setupDrawerItemSelected()
+        setupDrawerHeader()
+    }
+    private fun setupDrawerApplicationBar() {
+        // Afficher le menu hamburger sur la barre d'application
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        // Lier le tiroir de navigation à l'activité
+        // R.string.ouvert et R.string.ferme sont des strings de ressource.
+        // Référez-vous à la recette pour les strings de ressource pour voir comment les ajouter :
+        // https://info.cegepmontpetit.ca/3N5-Prog3/recettes/ressources-string
+        actionBarDrawerToggle = ActionBarDrawerToggle(this, binding.main, R.string.ouvert, R.string.ferme)
+
+        // Faire en sorte que le menu hamburger se transforme en flèche au clic, et vis versa
+        binding.main.addDrawerListener(actionBarDrawerToggle)
+        actionBarDrawerToggle.syncState()
+    }
+    private fun setupDrawerItemSelected() {
+        binding.nvTiroir.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.activity1 -> {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            false
+        }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        actionBarDrawerToggle.syncState()
+    }
+    private fun setupDrawerHeader() {
+        // Si on veut avoir du contenu dynamique dans l'en-tête,
+        val headerBinding: NavHeaderBinding = NavHeaderBinding.bind(binding.nvTiroir.getHeaderView(0))
+        val name = intent.getStringExtra("User")
+        headerBinding.username.text = name
+    }
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        actionBarDrawerToggle.onConfigurationChanged(newConfig)
+        super.onConfigurationChanged(newConfig)
     }
 }
